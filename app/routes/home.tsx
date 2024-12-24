bf97d28881ec16fcccdeffe1a3354f1bfe743c82
Istiak Ahmmed Akash <istiak_akash@outlook.com>
2024-12-24 15:15:20 +0600

    Add Tailwind CSS configuration and utility components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,15 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import { Button } from '@/components/ui/button';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className='flex flex-col items-center justify-center h-screen space-y-4'>
+      <h1 className='text-4xl'>Home Page for Guess Game</h1>
+      <Button>Start Game</Button>
+    </div>
+  );
 }
